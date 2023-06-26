cef92b2928142d35f12a667add70f6abcc3f62d8
yuuri <yuuri.okayama@synergy-effect.jp>
2023-06-26 10:08:22 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
     <x-slot name="header">ヘッダー１</x-slot>
 コンポーネントテスト１
 
-<x-tests.card title="タイトル" content="本文" :message="$message" />
+<x-tests.card title="タイトル1" content="本文" :message="$message" />
+<x-tests.card title="タイトル2" />
 </x-tests.app>
