229c1a410db2959ff60599d9a94f7fe21d78879f
yuuri <yuuri.okayama@synergy-effect.jp>
2023-08-04 22:28:09 +0900

    ルート、ビュー編集

--- routes/admin.php
+++ routes/admin.php
@@ -22,12 +22,12 @@ use App\Http\Controllers\Admin\OwnersController;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
 
 Route::resource('owners', OwnersController::class)
-->middleware('auth:admin');
+->middleware('auth:admin')->except(['show']);
 
 Route::prefix('expired-owners')->
     middleware('auth:admin')->group(function(){
