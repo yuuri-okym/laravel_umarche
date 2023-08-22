daab8386a3e1f44f361fa0df32453f8d3556110b
yuuri <yuuri.okayama@synergy-effect.jp>
2023-08-22 23:47:31 +0900

    intervention/image

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage; 
+use InterventionImage; 
 
 
 class ShopController extends Controller
@@ -54,7 +55,14 @@ class ShopController extends Controller
     {
         $imageFile = $request->image; //一時保存
         if(!is_null($imageFile) && $imageFile->isValid() ){
-        Storage::putFile('public/shops', $imageFile);
+        // Storage::putFile('public/shops', $imageFile);　リサイズなしの場合
+        $fileName = uniqid(rand().'_');
+        $extension = $imageFile->extension();
+        $fileNameToStore = $fileName. '.' . $extension;
+        $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+        
+        // dd($imageFile, $resizedImage);
+        Storage::put('public/shops/' . $fileNameToStore,$resizedImage );
         }
         
         return redirect()->route('owner.shops.index');
