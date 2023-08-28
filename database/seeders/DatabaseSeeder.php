e8821655bcaa80007f13a0e0c173db312518696a
yuuri <yuuri.okayama@synergy-effect.jp>
2023-08-28 11:27:08 +0900

    stock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            StockSeeder::class
         ]);
     }
 }
