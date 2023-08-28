c0264b1cc680c8187c751d3f4da99720e4452a1c
yuuri <yuuri.okayama@synergy-effect.jp>
2023-08-28 11:46:41 +0900

    category

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use App\Models\Product;
 use App\Models\Owner;
-use App\Models\SecondaryCategory;
+use App\Models\Shop;
+use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
@@ -51,7 +52,20 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())
+        ->select('id', 'name')
+        ->get();
+
+        $images = Image::where('owner_id', Auth::id())
+        ->select('id', 'title', 'filename')
+        ->orderBy('updated_at', 'desc')
+        ->get();
+
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
+
+        return view('owner.products.create',
+        compact('shops', 'images', 'categories'));
     }
 
     /**
