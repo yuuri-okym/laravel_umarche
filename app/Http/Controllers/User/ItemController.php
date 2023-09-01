471f6b7368b5e4d55ecdc7ba5ebdf3ec3b1b0035
yuuri <yuuri.okayama@synergy-effect.jp>
2023-09-01 23:43:06 +0900

    show調整

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,8 +9,14 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');  
+    }
+
     public function index()
     {
+
         $stocks = DB::table('t_stocks')
         ->select('product_id',
         DB::raw('sum(quantity) as quantity'))
@@ -39,5 +45,13 @@ class ItemController extends Controller
         // dd($stocks, $products);
         // $products = Product::all();
         return view('user.index', compact('products'));
+
     }
+
+    public function show($id)
+        {
+            $product = Product::findOrFail($id);
+
+            return view('user.show', compact('product'));
+        }
 }
