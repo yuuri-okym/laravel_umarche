243da9ed6781330fc05bd732ccadaddeeaa9b128
yuuri <yuuri.okayama@synergy-effect.jp>
2023-09-01 22:32:42 +0900

    product factory

---
+++ database/factories/StockFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+class StockFactory extends Factory
+{
+    // protected $model = Stock::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'product_id' => Product::factory(),
+            'type' => $this->faker->numberBetween(1,2),
+            'quantity' => $this->faker->randomNumber,
+        ];
+    }
+}
