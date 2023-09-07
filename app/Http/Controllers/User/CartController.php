63ad892607b7d81c5204f290f1ac9fc496672937
yuuri <yuuri.okayama@synergy-effect.jp>
2023-09-07 10:30:52 +0900

    stripe決済テスト

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -60,55 +60,51 @@ class CartController extends Controller
     {
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
-
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+ 
         $lineItems = [];
-        foreach($products as $product){
+        foreach($products as $product) {
             $quantity = '';
             $quantity = Stock::where('product_id', $product->id)->sum('quantity');
-
+ 
             if($product->pivot->quantity > $quantity){
-
                 return redirect()->route('user.cart.index');
-            
             } else {
-
-                $lineItem = [
+                $stripe_products = $stripe->products->create([
                     'name' => $product->name,
                     'description' => $product->information,
-                    'amount' => $product->price,
+                ]);
+                $stripe_price = $stripe->prices->create([
+                    'product' => $stripe_products,
+                    'unit_amount' => $product->price,
                     'currency' => 'jpy',
-                    'quantity' => $product->pivot->quantity, 
+                ]);
+                $lineItem = [
+                    'price' => $stripe_price,
+                    'quantity' => $product->pivot->quantity,
                 ];
                 array_push($lineItems, $lineItem);
-
             }
         }
         // dd($lineItems);
-        foreach($products as $peroduct){
+        // ストライプに渡す前に在庫を減らす
+        foreach($products as $product){
             Stock::create([
-                
-                    'product_id' => $product->id,
-                    'type' => \Constant::PRODUCT_LIST['reduce'],
-                    'quantity' => $product->pivot->quantity * -1,
-
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1,
             ]);
         }
-
-        dd('test');
-
-        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
-
-        $session = \Stripe\Checkout\Session::create([
-            'payment_method_types' => ['cart'],
+ 
+        $session = $stripe->checkout->sessions->create([
             'line_items' => [$lineItems],
             'mode' => 'payment',
             'success_url' => route('user.items.index'),
             'cancel_url' => route('user.cart.index'),
         ]);
-
-        $publicKey = enc('STRIPE_PUBLIC_KEY');
-
-        return view('user.checkout', 
-        compact('session', 'publicKey'));
+ 
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+ 
+        return view('user.checkout', compact('session', 'publicKey'));
     }
 }
