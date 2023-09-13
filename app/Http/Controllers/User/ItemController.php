5e562b3a1e0ba34881475b136fdfe48c76951746
yuuri <yuuri.okayama@synergy-effect.jp>
2023-09-13 22:41:57 +0900

    表示件数

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -36,7 +36,7 @@ class ItemController extends Controller
 
         $products = Product::availableItems()
         ->sortOrder($request->sort)
-        ->get();
+        ->paginate($request->pagination);
         
         return view('user.index', compact('products'));
 
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -37,8 +37,26 @@
                                 </option>
                             </select>
                         </div>
-
-                        <div>表示件数</div>
+                        <div>
+                            <span class="text-sm">表示件数</span><br>
+                            <select id="pagination" name="pagination">
+                             <option value="20"
+                             @if(\Request::get('pagination') === '20')
+                             selected
+                             @endif>20件
+                             </option>
+                             <option value="50"
+                             @if(\Request::get('pagination') === '50')
+                             selected
+                             @endif>50件
+                             </option>
+                             <option value="100"
+                             @if(\Request::get('pagination') === '100')
+                             selected
+                             @endif>100件
+                             </option>
+                            </select>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -49,24 +67,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="flex flex-wrap">
-                        
+                    <div class="flex flex-wrap">                 
                         @foreach ($products as $product)
                         <div class="w-1/4 p-2 md:p-4">
                         <a href="{{ route('user.items.show', ['item' => $product->id])}}">
-                         <div class="border rounded-md p-2 md:p-4">
-                             <x-thumbnail filename="{{ $product->filename ?? ''}}" type="products" />
-                                <div class="mt-4">
-                                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category }}</h3>
-                                    <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
-                                    <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
-                                </div>
-                         </div>   
+                            <div class="border rounded-md p-2 md:p-4">
+                                <x-thumbnail filename="{{ $product->filename ?? ''}}" type="products" />
+                                    <div class="mt-4">
+                                        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ $product->category }}</h3>
+                                        <h2 class="text-gray-900 title-font text-lg font-medium">{{ $product->name }}</h2>
+                                        <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm text-gray-700">円(税込)</span></p>
+                                    </div>
+                            </div>   
                         </a>
                         </div>
-                        @endforeach
-                        
-                     </div>
+                        @endforeach   
+                    </div>
+                    {{ $products->appends([
+                        'sort' => \Request::get('sort'),
+                        'pagination' => \Request::get('pagination')
+                    ])->links() }}
                 </div>
             </div>
         </div>
@@ -74,6 +94,10 @@
     <script>
         const select = document.getElementById('sort')
         select.addEventListener('change', function(){
+            this.form.submit()
+        })
+        const paginate = document.getElementById('pagination')
+        paginate.addEventListener('change', function(){
             this.form.submit()
         })
     </script>
