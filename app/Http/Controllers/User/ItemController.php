14bf730a13041d67c5395fd4e3225d69d1d6d908
yuuri <yuuri.okayama@synergy-effect.jp>
2023-09-14 10:19:41 +0900

    テストメール

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Auth;
 use App\Models\PrimaryCategory;
+use App\Mail\TestMail;
 
 class ItemController extends Controller
 {
@@ -35,6 +37,9 @@ class ItemController extends Controller
     public function index(Request $request)
     {
 
+        Mail::to('test@example.com')
+        ->send(new TestMail());
+        
         $categories = PrimaryCategory::with('secondary')
         ->get();
 
